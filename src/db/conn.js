9011f5b3683d8081e753b6bfab003c20fbb6dd40
akashdnb <akashranjandnb@gmail.com>
2023-02-28 22:05:36 +0530

    removed unique bug

--- src/db/conn.js
+++ src/db/conn.js
@@ -5,7 +5,7 @@ const conn_string = process.env.CONN_STRING;
     mongoose.connect(conn_string , {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true, 
+      autoIndex: true, 
      }).then(()=>{
       console.log('connected to database!!')
      }).catch((e)=>{
